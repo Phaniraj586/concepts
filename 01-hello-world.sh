b70a3be432226631af2a2eb4e0c0fe96f84745b1
Phaniraj <phaniraj586@gmail.com>
2023-10-12 23:58:21 +0530

    ShellScript-Practice1-variables

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hi, Hello team. I am learning DevOps with AWS"
+
+echo "Ramesh: Hi suresh, Good Morning"
+echo "Suresh : Hi Ramesh, Good Morning"
+echo "Ramesh : How are you Suresh"
+echo "Suresh : i am good, how are you ramesh"
