155efe7b3ce7755f9dd6e5ad44a06c1755e26951
Phaniraj <phaniraj586@gmail.com>
2023-10-13 13:55:47 +0530

    Logic to install git as Root user

---
+++ 08-install-git.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# check if the user is root user or not
+# 'id -u' will provide user id & for root user it is always "0"
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR : Please run the script with the root access"
+else
+
+yum install git -y
+
+#
+#
+#
+
