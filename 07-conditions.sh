328b87c6d75622bf423b273a5dc83438e78a85ac
Phaniraj <phaniraj586@gmail.com>
2023-10-13 13:45:36 +0530

    variable defining

---
+++ 07-conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NUMBER=$1 # Taking user input from the command line
+
+if [$NUMBER -gt 20 ]
+echo "given number $NUMBER is greater than 20"
+else 
+echo "given number $NUMBER is not greater than 20"
+fi 
