c58d7443df767d5334d769ee4c0797f764f4d25c
Phaniraj <phaniraj586@gmail.com>
2023-10-13 17:27:05 +0530

    Installing MYSQL & POSTFIX with function

---
+++ 09-functions-redirection.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+VALIDATE(){
+    if[ $1 -ne 0 ]
+    then
+        echo "$2...FAILUER"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
+USERID=$(id -u)
+
+if[ $USERID -ne 0 ]
+then    
+    echo "ERROR: Please run this sript with root access"
+    exit 1
+fi
+
+yum install mysql -y &>>$LOGFILE
+
+VALIDATE $? "Installing mysql"
+
+yum install postfix -y &>>$LOGFILE
+
+VALIDATE $? "installing postfix"
