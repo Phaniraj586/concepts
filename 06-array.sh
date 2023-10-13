d79e5c9039806dae6fe0abfc1d8d5ed349e0e877
Phaniraj <phaniraj586@gmail.com>
2023-10-13 19:02:28 +0530

    Installing MYSQL & POSTFIX with function

--- 06-array.sh
+++ 06-array.sh
@@ -3,3 +3,8 @@
 PERSON=("Ramesh" "Suresh" "Sachin") # this is called array for holding multiple values
 echo "first person is: ${PERSON[0]}" # reading the array based on index
 echo "All the persons in array are: ${PERSON[@]} " # reading all persons from the array
+ 
+
+ APPLICATION=("git" "postfix" "cyrus-sasl-plain" "mailx")
+n=$(${APPLICATION[@]})
+echo "No of items are: $n"
