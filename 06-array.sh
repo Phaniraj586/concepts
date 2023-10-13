09a403b8053638ed7e76d9986f54dca7ec30ac1b
Phaniraj <phaniraj586@gmail.com>
2023-10-13 13:14:04 +0530

    variable defining

--- 06-array.sh
+++ 06-array.sh
@@ -2,3 +2,4 @@
 
 PERSON=("Ramesh" "Suresh" "Sachin") # this is called array for holding multiple values
 echo "first person is: ${PERSON[0]}" # reading the array based on index
+echo "All the persons in array are: ${PERSON[@]} " # reading all persons from the array
