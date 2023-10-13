8b5588cb049b981b3288d28c00bf0f5757b81978
Phaniraj <phaniraj586@gmail.com>
2023-10-13 12:44:29 +0530

    variable defining

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is the comment; here we need to develop program to add 2 numbers
+
+NUMBER1=$1
+NUMBER2=$2
+
+# run a command inside shell to add these numbers
+SUM=$($NUMBER1+$NUMBER2)
+echo "$SUM"
