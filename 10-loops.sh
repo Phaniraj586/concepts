d1677a96d64ccc6d456a27119150bc05ec8d84a6
Phaniraj <phaniraj586@gmail.com>
2023-10-13 18:50:36 +0530

    Installing MYSQL & POSTFIX with function

--- 10-loops.sh
+++ 10-loops.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 
-for i in {1..100}
-do 
-echo $i 
+#Model as below
+# for i in {1..100}
+# do 
+# echo $i 
+# done
+
+#Installing git postfix cyrus-sasl-plain mailx
+
+APPLICATION=("git" "postfix" "cyrus-sasl-plain" "mailx")
+
+ R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILUER $N" 
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then    
+    echo -e "$R ERROR: Please run this sript with root access $N"
+    exit 1
+fi
+
+for i in (1..$#)
+do
+    yum install ${APPLICATION[i]} -y &>>$LOGFILE
+    VALIDATE $? "Installing ${APPLICATION[i]}"
 done
