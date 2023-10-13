4e7afeac56e7576df63e216e9579fb76a72ad826
Phaniraj <phaniraj586@gmail.com>
2023-10-13 12:38:42 +0530

    variable defining

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,3 +3,9 @@
 DATE=$(date)
 
 echo "Hello, this script is executed at timestamp: $DATE"
+PERSON1=$1 # this is the first value from command line
+PERSON2=$2 # this is the second value from command line 
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2 : Hi $PERSON1, Good Morning"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 : i am good, how are you $PERSON1"
