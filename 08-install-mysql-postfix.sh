e5af537d8e46743ded7f8b8126f21823c99e7d34
Phaniraj <phaniraj586@gmail.com>
2023-10-13 15:02:57 +0530

    Installing MYSQL & POSTFIX

---
+++ 08-install-mysql-postfix.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Checking root access
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then    
+    echo "Please run the script with root access"
+    exit 1
+fi
+
+# Installing MYSQL 
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo " Installation of MYSQL is not successful, error code is $?"
+    exit 1
+else
+    echo "Installation of MYSQL is successful"
+fi
+
+# Installing postfix
+
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then
+    echo " Installation of POSTFIX is not successful, error code is $?"
+    exit 1
+else
+    echo "Installation of POSTIFX is successful"
+fi
